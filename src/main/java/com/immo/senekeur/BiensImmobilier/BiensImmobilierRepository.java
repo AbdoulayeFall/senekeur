f691ad6eab154fa01e047038c8bba576ca724c02
AbdoulayeFall <abdoulaye.f@hotmail.com>
2021-05-29 10:35:00 -0400

    Implémentation des classes des biens de l'immobilier

---
+++ src/main/java/com/immo/senekeur/BiensImmobilier/BiensImmobilierRepository.java
@@ -0,0 +1,7 @@
+package com.immo.senekeur.BiensImmobilier;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BiensImmobilierRepository extends CrudRepository<BiensImmobilier, Integer>{
+    
+}
---
+++ src/main/java/com/immo/senekeur/BiensImmobilier/Maison/Maison.java
@@ -0,0 +1,31 @@
+package com.immo.senekeur.BiensImmobilier.Maison;
+import com.immo.senekeur.BiensImmobilier.BiensImmobilier;
+
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+@Entity
+@DiscriminatorValue("MAISON")
+public class Maison extends BiensImmobilier{
+    
+    private Integer nombre_chambre;
+
+    public Maison() {
+        super();
+    }
+    public Maison(Integer id, String region, String ville) {
+        super(id, region, ville);
+    }
+
+    public Integer getChambres() {
+        return nombre_chambre;
+    }
+
+    public void setNombreChambres(Integer nombre_chambre) {
+        this.nombre_chambre = nombre_chambre;
+    }
+
+
+}
